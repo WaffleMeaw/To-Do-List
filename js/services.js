3b5e21b8921238560fdd2adce307ac72d7e22bc2
WaffleMeaw <5535512132@psu.ac.th>
2017-02-25 17:39:37 +0700

    Update some logic in service.

--- js/services.js
+++ js/services.js
@@ -3,53 +3,75 @@ var app = angular.module('ToDoList.Services',[]);
 app.service('DBservice', function(){
   this.database = firebase.database();
 
-  this.GetData = function() {
+  // ========== GET DATA FROM DATABASE =========================================
+  this.GetData = function(){
     return firebase.database().ref('tasks/').once('value');
   };
 
-  this.PostData = function(taskID, titleNew, descriptionNew, checkNew) {
-    var currentDate = new Date();
-    console.log(currentDate);
-    currentDate = currentDate.toLocaleString();
+  // ========== POST DATA TO DATABASE ==========================================
+  this.PostData = function(taskID, title, description, dateTime, timestamp, check){
 
-    if(titleNew == null || titleNew == ''){
-      titleNew = '-';
-    }else if(descriptionNew == null || descriptionNew == ''){
-      descriptionNew = '-';
-    }else if(checkNew == null || checkNew == ''){
-      checkNew = false ;
-    }else{}
+    if(timestamp == null || timestamp == ''){
+      var timestamp = new Date();
+      timestamp = timestamp.toLocaleString();
+    }
+    else if(dateTime == null || dateTime == ''){
+      dateTime = '-';
+    }
+    else if(title == null || title == ''){
+      title = '-';
+    }
+    else if(description == null || description == ''){
+      description = '-';
+    }
+    else if(check == null || check == ''){
+      check = false ;
+    }
+    else{}
 
-    return firebase.database().ref('tasks/' + taskID).set({
-      title: titleNew,
-      description: descriptionNew,
-      timestamp: currentDate,
-      check: checkNew
-    });
-  };
-
-  this.UpdateData = function(taskID, titleUpdate, descriptionUpdate, checkUpdate) {
-
-    if(titleUpdate == null || titleUpdate == ''){
-      titleUpdate = '';
-    }else if(descriptionUpdate == null || descriptionUpdate == ''){
-      descriptionUpdate = '-';
-    }else if(checkUpdate == null || checkUpdate == ''){
-      checkUpdate = false;
-    }else{}
+    // console.log(dateTime);
 
     var data = {
-      title: titleUpdate,
-      description: descriptionUpdate,
-      check: checkUpdate
+      taskID: taskID,
+      title: title,
+      description: description,
+      dateTime: dateTime,
+      timestamp: timestamp,
+      check: check
     };
+    // console.log(JSON.stringify(data));
+    return firebase.database().ref('tasks/' + taskID).set(data);
+  };
+
+  // ========== UPDATE DATA TO DATABASE ========================================
+  this.UpdateData = function(taskID, title, description, dateTime, timestamp, check) {
+
+    // if(titleUpdate == null || titleUpdate == ''){
+    //   titleUpdate = '';
+    // }else if(descriptionUpdate == null || descriptionUpdate == ''){
+    //   descriptionUpdate = '-';
+    // }else if(checkUpdate == null || checkUpdate == ''){
+    //   checkUpdate = false;
+    // }else{}
+
+    var data = {
+      taskID: taskID,
+      title: title,
+      description: description,
+      dateTime: dateTime,
+      timestamp: timestamp,
+      check: check
+    };
+
+    console.log(JSON.stringify(data));
     var updates = {};
-    updates['/tasks/' + taskID] = data;
+    updates['/tasks/' + data.taskID] = data;
 
     return firebase.database().ref().update(updates);
   };
 
-  this.DeleteData = function(taskID) {
+  // ========== POST DATA TO DATABASE ==========================================
+  this.DeleteData = function(taskID){
       console.log("in service : " + taskID);
       return firebase.database().ref('/tasks/' + taskID).remove();
   };
