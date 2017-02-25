1ceb59fd4ee1d2a66314d9dc366556f71c8d52ff
WaffleMeaw <5535512132@psu.ac.th>
2017-02-25 17:38:44 +0700

    Import new library to app.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 angular.module('ToDoList', ['ToDoList.Services',
                             'ToDoList.Controllers',
-                            'ui.router'])
+                            'ui.router',
+                            'ui.bootstrap.datetimepicker'])
 
 .run(function ($state, $rootScope) {
   // ========== For switch button on navbar ==========
@@ -18,15 +19,22 @@ angular.module('ToDoList', ['ToDoList.Services',
     })
     .state('todo', {
         url: '/todo',
-        templateUrl: 'template/todo.html'
+        templateUrl: 'template/todo.html',
+        controller: 'TodoCtrl'
     })
     .state('completed', {
         url: '/completed',
-        templateUrl: 'template/completed.html'
+        templateUrl: 'template/completed.html',
+        controller: 'TodoCtrl'
     })
     .state('create', {
         url: '/create',
         templateUrl: 'template/create.html',
+        controller: 'CreateCtrl'
+    })
+    .state('entry', {
+        url: '/entry',
+        templateUrl: 'template/entry.html',
         controller: 'CreateCtrl'
     });
   $urlRouterProvider.otherwise('/all');
