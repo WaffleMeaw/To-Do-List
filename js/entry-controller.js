357423eaf486ceed32be1a3176ca9381b33d9f19
WaffleMeaw <5535512132@psu.ac.th>
2017-02-27 17:55:43 +0700

    new controller for edit data.

---
+++ js/entry-controller.js
@@ -0,0 +1,54 @@
+angular.module('ToDoList')
+
+.controller('EntryCtrl', function(DBservice, $scope, $state, $stateParams){
+
+  $scope.dataEntry = [];
+  function GetOneData(){
+    DBservice.GetOneData($stateParams.id)
+      .then(function(respons){
+          $scope.dataEntry = respons.val();
+          $scope.$apply();
+          // console.log($scope.dataEntry);
+      }, function(error){
+          // console.log(error);
+      });
+  };
+  GetOneData();
+
+  $scope.updateEntry = function(){
+
+    if($scope.dataEntry.title == null || $scope.dataEntry.title == ''){
+      $scope.dataEntry.title = '-';
+    }
+    else if($scope.dataEntry.description == null || $scope.dataEntry.description == ''){
+      $scope.dataEntry.description = '-';
+    }
+    else if($scope.dataEntry.check == null || $scope.dataEntry.check == ''){
+      $scope.dataEntry.check = false ;
+    }
+    else{}
+
+    var dateTime = $scope.dataEntry.dateTime;
+    if(dateTime == null){
+      dateTime = '';
+    }else{
+      dateTime = dateTime.toLocaleString();
+    };
+
+    DBservice.UpdateData($scope.dataEntry.taskID,
+                         $scope.dataEntry.title,
+                         $scope.dataEntry.description,
+                         dateTime,
+                         $scope.dataEntry.timestamp,
+                         $scope.dataEntry.check)
+      .then(function(respons){
+          // console.log(respons);
+          $scope.dataEntry = [];
+          GetOneData();
+          $state.go('entry', {id: $stateParams.id});
+      }, function(error){
+          // console.log(error);
+      });
+  };
+
+});
